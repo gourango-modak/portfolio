0475abafe323adc11afc80d1158c9c59ec7d1fb5
Gourango Modak <gourango.modak@enosisbd.com>
2025-08-29 18:31:21 +0600

    Update codeblock

--- src/components/editorBlocks/CodeBlock.jsx
+++ src/components/editorBlocks/CodeBlock.jsx
@@ -1,24 +1,29 @@
 import { useEffect, useRef } from "react";
 import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-python";
 
 const CodeBlock = ({ code, language }) => {
     const ref = useRef();
 
     useEffect(() => {
-        if (ref.current) Prism.highlightElement(ref.current);
+        if (ref.current) {
+            Prism.highlightElement(ref.current);
+        }
     }, [code, language]);
 
     return (
-        <div className="relative my-6 rounded-lg not-prose bg-slate-800 text-gray-100">
+        <div className="relative my-6 not-prose">
             {/* Left accent line */}
             <div className="absolute left-0 top-0 bottom-0 w-1.5 bg-emerald-500 rounded-l-lg"></div>
 
             <pre
-                className={`language-${language} overflow-x-auto whitespace-pre p-4 sm:p-6 rounded-lg hide-scrollbar`}
+                className={`language-${language} overflow-x-auto whitespace-pre p-6 rounded-lg hide-scrollbar`}
             >
                 <code
                     ref={ref}
-                    className={`language-${language} font-mono text-sm leading-relaxed block min-w-full`}
+                    className={`language-${language} font-mono text-sm leading-relaxed block min-w-full py-8 px-4`}
                 >
                     {code}
                 </code>
