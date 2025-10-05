bb16eb05dcb70dc355af27fdf90799c5b74018e1
Gourango Modak <gourango.modak@enosisbd.com>
2025-10-05 19:33:26 +0600

    Remove temporary drawing app logics

--- src/components/vectorDrawV1/tools/toolRegistry.js
+++
@@ -1,7 +0,0 @@
-import { lineTool } from "./lineTool";
-import { arrowTool } from "./arrowTool";
-
-export const toolRegistry = {
-    [lineTool.name]: lineTool,
-    [arrowTool.name]: arrowTool,
-};
