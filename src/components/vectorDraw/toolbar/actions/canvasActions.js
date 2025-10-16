c666d689154e3d218ee066c36b056e52f1cd6dea
Gourango Modak <gourango.modak@enosisbd.com>
2025-10-17 00:35:10 +0600

    Fixed - In paged canvas mode, pages get deleted upon clear all

--- src/components/vectorDraw/toolbar/actions/canvasActions.js
+++ src/components/vectorDraw/toolbar/actions/canvasActions.js
@@ -3,12 +3,15 @@ import {
     canvasPropertiesSlice,
     frameSlice,
 } from "../../store/utils";
+import { isPagedCanvasMode } from "../../utils/canvasUtils";
 import { TOOL_ACTION_TYPES } from "../config/toolActionTypes";
 
 export const canvasActions = {
     [TOOL_ACTION_TYPES.CLEAR_CANVAS]: () => {
         shapeSlice.getSlice().reset();
-        frameSlice.getSlice().reset();
+        if (!isPagedCanvasMode()) {
+            frameSlice.getSlice().reset();
+        }
     },
     [TOOL_ACTION_TYPES.RESET_ZOOM_AND_PAN]: () => {
         canvasPropertiesSlice.getSlice().resetViewport();
