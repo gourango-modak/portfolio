4276a671371e85f719786704a7a06c5ed8af5094
Gourango Modak <gourango.modak@enosisbd.com>
2025-11-08 13:49:24 +0600

    Fixed image height issue

--- src/components/EditorJs/Blocks/EditorJsImageBlock.jsx
+++ src/components/EditorJs/Blocks/EditorJsImageBlock.jsx
@@ -24,7 +24,7 @@ const EditorJsImageBlock = ({
                 src={url}
                 alt={extractOriginalFileName(name)}
                 className="object-contain"
-                style={{ width, height }}
+                style={{ width, height: "auto" }}
             />
         </div>
     );
